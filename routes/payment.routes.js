fc7391702ff63304e5d02e75abb97a5fb0e12b0d
MaelleV <69573791+MaelleVo@users.noreply.github.com>
2024-06-18 20:24:34 +0200

    update

---
+++ routes/payment.routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+// ROUTER EXPRESS
+const router = express.Router();
+
+app.post("/payment", async (req, res) => {
+  try {
+    // On crée une intention de paiement
+    const paymentIntent = await stripe.paymentIntents.create({
+      // Montant de la transaction
+      amount: 2000,
+      // Devise de la transaction
+      currency: "usd",
+      // Description du produit
+      description: "La description du produit",
+    });
+    // On renvoie les informations de l'intention de paiement au client
+    res.json(paymentIntent);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
